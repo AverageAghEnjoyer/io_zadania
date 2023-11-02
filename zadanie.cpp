1025fba1cd7798bfd5d8c07f5f435bbde90d89b6
AverageAghEnjoyer <148354031+AverageAghEnjoyer@users.noreply.github.com>
2023-11-02 19:17:34 +0100

    Update zadanie.cpp

--- zadanie.cpp
+++ zadanie.cpp
@@ -1,82 +1,77 @@
-
-// Online C++ Compiler - Build, Compile and Run your C++ programs online in your favorite browser
-
-#include<iostream>
-#include <vector>
+#include <iostream>
 #include <algorithm>
 
 using namespace std;
 
-class Product{
-public:
+struct Product {
     string name;
-    int price;
-
-        Product(){
-            
-        }
-        Product(string n, int p){
-            name = n;
-            price = p;
-        }
-        void showInfo(){
-            cout << "name: " << name << ", price: " << price << endl;
-        }
+    double price;
 };
 
-bool compare(Product a, Product b){
-    return a.price < b.price;
+bool compareProducts(const Product& a, const Product& b) {
+    return a.price > b.price;
 }
 
-int main()
-{
-     // 1
-    //int AA[10] = {0, 1, 2, 3, 4, 5, -6, -7, -8, -9};
-    const int items = 3;
-    int AA[items];
-    for (int i = 0; i < 3; i++){
-        int number;
-        cout << "enter number: ";
-        cin >> number;
-        AA[i] = number;
-    }
-    int *BB = new int[items];
-    int counter = 0;
-    for (int i = 0; i < items; i++){
-        if (AA[i] < 0){
-            BB[counter] = AA[i];
-            counter++;
-        }  
-    }
-    cout << "negative numbers: ";
-    
-   
-    for (int i = 0; i < counter; i++){
-        cout << BB[i] << " ";
-    }
-    
-    cout << endl << "skipped numbers: " << items - counter << endl << endl;
-    
-    delete BB;
-    
+int main() {
     // 2
-    // Product tab[10];
-    vector <Product> tab;
-    counter = 0;
-    for (int i = 0; i < 3; i++){
-        string name;
-        int price;
-        cout << "enter name: ";
-        cin >> name;
-        cout << "enter price: ";
-        cin >> price;
-        tab[i] = Product(name, price);
+    const int size = 3;
+    int AA[size];
+    int* BB = nullptr;
+    int countNegative = 0;
+
+    cout << "Podaj 10 liczb:" << endl;
+    for (int i = 0; i < size; i++) {
+        cin >> AA[i];
+        if (AA[i] < 0) {
+            countNegative++;
+        }
     }
-    sort( tab.begin(), tab.end(), compare);
-    for (int i = 0; i < 10; i++){
-        tab[i].showInfo();
+
+    if (countNegative > 0) {
+        BB = new int[countNegative];
+        int index = 0;
+
+        for (int i = 0; i < size; i++) {
+            if (AA[i] < 0) {
+                BB[index] = AA[i];
+                index++;
+            }
+        }
     }
-    cout << "done" << endl;
+
+    cout << "Liczby ujemne: ";
+    if (countNegative > 0) {
+        for (int i = 0; i < countNegative; i++) {
+            cout << BB[i] << " ";
+        }
+        cout << endl;
+        cout << "Ilosc liczb ujemnych: " << countNegative << endl;
+        cout << "Ilosc liczb pominietych: " << (size - countNegative) << endl;
+    } else {
+        cout << "Brak liczb ujemnych." << endl;
+    }
+
+    if (BB != nullptr) {
+        delete[] BB;
+    }
+
+    // 4
+    const int numProducts = 3;
+    Product products[numProducts];
+
+    for (int i = 0; i < numProducts; i++) {
+        cout << "Podaj nazwe " << (i + 1) << ": ";
+        cin >> products[i].name;
+        cout << "Podaj cene " << (i + 1) << ": ";
+        cin >> products[i].price;
+    }
+
+    sort(products, products + numProducts, compareProducts);
+
+    cout << "Produkty od najdrozszych:" << endl;
+    for (int i = 0; i < numProducts; i++) {
+        cout << "Nazwa: " << products[i].name << ", Cena: " << products[i].price << endl;
+    }
+
     return 0;
 }
-
